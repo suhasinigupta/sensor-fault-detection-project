816370f54183d3450c5fbcd9408fc76f5124cada
unknown <suhasinigupta31@gmail.com>
2023-03-15 22:06:34 +0530

    Removed

--- sensor/configuration/mongodb_connection.py
+++ sensor/configuration/mongodb_connection.py
@@ -14,7 +14,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url ="mongodb+srv://sgupta:suhasini@cluster0.lyxwcmf.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url =""
                 print(mongo_db_url)
                 if "localhost" in mongo_db_url:
                     MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
